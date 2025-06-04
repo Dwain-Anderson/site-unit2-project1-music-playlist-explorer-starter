70372a4507dd11edafb5d1f688f4332035ea0a7d
Dwain Kyle Anderson <dwainanderson@meta.com>
2025-06-04 07:25:40 -0700

    Initial commit & [feature] Basic HTML Skeleton including modal but with no CSS styling yet

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,28 @@
+const modal = document.getElementById("playlist-card-modal");
+const span = document.getElementsByClassName("close")[0];
 
+function openModal(playlistDetails) {
+   document.getElementById('playlist-title').innerText = playlistDetails.title;
+   document.getElementById('playlist-cover-image').src = playlistDetails.imageUrl;
+   document.getElementById('playlist-creator-name').innerText = playlistDetails.creatorName;
+   document.getElementById('song-title').innerText =  playlistDetails.songs[0].title;
+   document.getElementById('song-image').src = playlistDetails.songs[0].imageUrl;
+   document.getElementById('song-artist-name').innerText =  playlistDetails.songs[0].artistName;
+   document.getElementById('song-album-name').innerText = playlistDetails.songs[0].albumName;
+   document.getElementById('song-duration').innerText =  playlistDetails.songs[0].duration;
+   modal.style.display = "block";
+}
+
+
+
+
+
+
+span.onclick = function() {
+   modal.style.display = "none";
+}
+window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+}
