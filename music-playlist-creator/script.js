28009f4c3ecedee9bd960753810654e2c067be82
Dwain Kyle Anderson <dwainanderson@meta.com>
2025-06-04 11:16:45 -0700

    Progress with basic css styling to match wireframe, moving on to dynamically rendering now.

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -5,24 +5,30 @@ function openModal(playlistDetails) {
    document.getElementById('playlist-title').innerText = playlistDetails.title;
    document.getElementById('playlist-cover-image').src = playlistDetails.imageUrl;
    document.getElementById('playlist-creator-name').innerText = playlistDetails.creatorName;
+   
+   // change later today/tommorow to get collection of elements by class for when we dynamically render the songs list.
+   // iterate over the list of dom element pointers and assignment them to each attribute
+
    document.getElementById('song-title').innerText =  playlistDetails.songs[0].title;
    document.getElementById('song-image').src = playlistDetails.songs[0].imageUrl;
    document.getElementById('song-artist-name').innerText =  playlistDetails.songs[0].artistName;
    document.getElementById('song-album-name').innerText = playlistDetails.songs[0].albumName;
    document.getElementById('song-duration').innerText =  playlistDetails.songs[0].duration;
    modal.style.display = "block";
+
 }
-
-
-
-
-
 
 span.onclick = function() {
    modal.style.display = "none";
 }
+
 window.onclick = function(event) {
    if (event.target == modal) {
       modal.style.display = "none";
    }
 }
+
+
+
+
+
